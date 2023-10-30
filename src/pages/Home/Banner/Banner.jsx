0d2570df9eadb6b17f46fdbda1c1a9743f49c5b6
Jahirul83 <jahirulislamcse83.com>
2023-10-30 20:59:09 +0600

    About Us page design

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -14,8 +14,8 @@ const Banner = () => {
                         <h3 className='text-6xl font-bold'>Affordable Price For Car Servicing</h3>
                         <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
                         <div>
-                            <button className="btn btn-active btn-primary mr-5">Discover More</button>
-                            <button className="btn btn-outline btn-secondary">Latest Projects</button>
+                            <button className="btn btn-active bg-[#FF3811] text-white mr-5">Discover More</button>
+                            <button className="btn btn-outline text-white">Latest Projects</button>
                         </div>
                     </div>
                 </div>
@@ -25,14 +25,14 @@ const Banner = () => {
                 </div>
             </div>
             <div id="slide2" className="carousel-item relative w-full">
-            <img src={Img2} className="w-full rounded-lg" />
+                <img src={Img2} className="w-full rounded-lg" />
                 <div className="absolute rounded-lg flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
                     <div className='text-white space-y-7 pl-12 w-1/2'>
                         <h3 className='text-6xl font-bold'>Affordable Price For Car Servicing</h3>
                         <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
                         <div>
-                            <button className="btn btn-active btn-primary mr-5">Discover More</button>
-                            <button className="btn btn-outline btn-secondary">Latest Projects</button>
+                            <button className="btn btn-active bg-[#FF3811] text-white mr-5">Discover More</button>
+                            <button className="btn btn-outline text-white">Latest Projects</button>
                         </div>
                     </div>
                 </div>
@@ -42,14 +42,14 @@ const Banner = () => {
                 </div>
             </div>
             <div id="slide3" className="carousel-item relative w-full">
-            <img src={Img3} className="w-full rounded-lg" />
+                <img src={Img3} className="w-full rounded-lg" />
                 <div className="absolute rounded-lg flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
                     <div className='text-white space-y-7 pl-12 w-1/2'>
                         <h3 className='text-6xl font-bold'>Affordable Price For Car Servicing</h3>
                         <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
                         <div>
-                            <button className="btn btn-active btn-primary mr-5">Discover More</button>
-                            <button className="btn btn-outline btn-secondary">Latest Projects</button>
+                            <button className="btn btn-active bg-[#FF3811] text-white mr-5">Discover More</button>
+                            <button className="btn btn-outline text-white">Latest Projects</button>
                         </div>
                     </div>
                 </div>
@@ -59,14 +59,14 @@ const Banner = () => {
                 </div>
             </div>
             <div id="slide4" className="carousel-item relative w-full">
-            <img src={Img4} className="w-full rounded-lg" />
+                <img src={Img4} className="w-full rounded-lg" />
                 <div className="absolute rounded-lg flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
                     <div className='text-white space-y-7 pl-12 w-1/2'>
                         <h3 className='text-6xl font-bold'>Affordable Price For Car Servicing</h3>
                         <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
                         <div>
-                            <button className="btn btn-active btn-primary mr-5">Discover More</button>
-                            <button className="btn btn-outline btn-secondary">Latest Projects</button>
+                            <button className="btn btn-active bg-[#FF3811] text-white mr-5">Discover More</button>
+                            <button className="btn btn-outline text-white">Latest Projects</button>
                         </div>
                     </div>
                 </div>
