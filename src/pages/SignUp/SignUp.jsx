306c746775575ae141a0e3e77833f1eb670c19c7
Jahirul83 <jahirulislamcse83@gmail.com>
2023-10-30 22:20:47 +0600

    login and signUp page created and firebase cofig file created

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom';
 import img from '../../assets/images/login/login.svg'
 
-const Login = () => {
-    const handleLogin = event => {
+const SignUp = () => {
+    const handleSignUp = event => {
         event.preventDefault();
 
-        
+
     }
     return (
         <div className="hero min-h-screen bg-base-200">
@@ -15,8 +15,14 @@ const Login = () => {
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
-                        <h1 className="text-3xl text-center font-bold">Login</h1>
-                        <form onSubmit={handleLogin}>
+                        <h1 className="text-3xl text-center font-bold">Sign Up</h1>
+                        <form onSubmit={handleSignUp}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name='name' placeholder="Name" className="input input-bordered" required />
+                            </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
@@ -25,7 +31,7 @@ const Login = () => {
                             </div>
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">Password</span>
+                                    <span className="label-text">Confirm Password</span>
                                 </label>
                                 <input type="password" name='password' placeholder="password" className="input input-bordered" required />
                                 <label className="label">
@@ -33,10 +39,10 @@ const Login = () => {
                                 </label>
                             </div>
                             <div className="form-control mt-6">
-                                <input className="btn btn-primary" type="submit" value="Login" />
+                                <input className="btn btn-primary" type="submit" value="Sign Up" />
                             </div>
                         </form>
-                        <p className='my-4 text-center'>New to the website <Link className='text-orange-600 font-bold' to='/signup'>Sign Up</Link></p>
+                        <p className='my-4 text-center'>Already have an account <Link className='text-orange-600 font-bold' to='/login'>Login</Link></p>
                     </div>
                 </div>
             </div>
@@ -44,4 +50,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default SignUp;
